053c1ed836315e009729f1de0635b4d16630a16c
Gabriel Neri <mneri74@gmail.com>
2026-07-14 13:44:19 -0300

    queda das palavras

--- jogo.c
+++ jogo.c
@@ -23,16 +23,22 @@
 #define BRANCO 15
 
 // A função gotoxy serve para posicionar o cursor do terminal na coluna x e linha y permitindo mudar o local onde os printf vão ocorrer
-void gotoxy(int x, int y) {
+int gotoxy(int x, int y) {
     if( x>=0 && y>=0){
         COORD coord;
         coord.X = x;
         coord.Y = y;
         SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+        return 0;
     }
+    return 1;
 }
 void textcolor(int color) {
     SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color);
+}
+
+int setup(int x[], int y[]){
+
 }
 
 int main() {
@@ -54,12 +60,8 @@ int main() {
         GetConsoleScreenBufferInfo(h, &info);
         int x= (info.dwSize.X)/2; 
         int y =(info.dwSize.Y)/2;
-    while (1)
-    {
-        int tempo= clock();
-        tempo=tempo/CLOCKS_PER_SEC;
-        // função de esclher palavras e coloca-las no char
-       for (int i=0; i<3; i++){
+    setup(coordX, coordY);
+    for (int i=0; i<3; i++){
             gotoxy(x,y);
             textcolor(VERMELHO_CLARO);
             printf("ONDA %d", onda);
@@ -69,12 +71,22 @@ int main() {
             printf("onda %d", onda);
             Sleep(500);
     }
+        while (1)
+    {
+        int tempo= clock(), timer=0;
+        tempo=tempo/CLOCKS_PER_SEC;
+        // função de esclher palavras e coloca-las no char
         textcolor(PADRAO);
         gotoxy(x,y);
         printf("                                     ");
-        /*coordX[controle]=rand()/(x*2-strlen(palavras[controle]));
-        coordY[controle]=0;*/
-    
+        if((tempo-timer)> 1){
+        for (controle; controle<26; controle++){
+            if (gotoxy(coordX[controle], coordY[controle])== 0){
+                // if pra verificar se a palavra a ser atualizada é a palavra que o jogador está digitando
+                printf("%s", palavras[controle]);
+                } 
+            }   
+        }  
     }
     
     
