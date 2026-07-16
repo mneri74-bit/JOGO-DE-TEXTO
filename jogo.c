813ba1e9435d14eba5b413599791891a68a383c8
Gabriel Neri <mneri74@gmail.com>
2026-07-16 13:34:30 -0300

    parte do jogo
    ajustes na UI do jogo e programação de queda e de vidas

--- jogo.c
+++ jogo.c
@@ -37,8 +37,7 @@ void textcolor(int color) {
     SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color);
 }
 
-int setup(int onda,int x[], int y[], char matriz[26][50]){
-    srand(time(NULL));
+void setup(int onda,int x[], int y[], char matriz[26][50], int meiox, int meioy){
     HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
         CONSOLE_SCREEN_BUFFER_INFO info;
         GetConsoleScreenBufferInfo(h, &info);
@@ -49,61 +48,114 @@ int setup(int onda,int x[], int y[], char matriz[26][50]){
         if (x[i]){
 
         }
+    }
+
         for (int i=0; i<3; i++){
-        gotoxy(x,y);
+        gotoxy(meiox, meioy);
         textcolor(VERMELHO_ESCURO);
         printf("ONDA %d", onda);
         Sleep(500);
-        gotoxy(x,y);
+        gotoxy(meiox, meioy);
         textcolor(VERMELHO_CLARO);
         printf("onda %d", onda);
         Sleep(500);
         }
         textcolor(PADRAO);
-            gotoxy(x,y);
+        gotoxy(meiox, meioy);
             printf("       ");
-    }
 }
+//<3 
+void placar(int display, int score, int onda, int vidas){
+    HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
+        CONSOLE_SCREEN_BUFFER_INFO info;
+        GetConsoleScreenBufferInfo(h, &info); 
+    gotoxy(0, info.dwSize.Y-3);
+    textcolor(VERMELHO_CLARO);
+    if (display){
+        for (int i=0; i<info.dwSize.X; i++){
+            printf("#");}}
+    gotoxy(0, info.dwSize.Y-2);
+    printf("score: %d", score);
+    gotoxy(info.dwSize.X/2, info.dwSize.Y-2);
+    printf("onda %d", onda);
+    gotoxy(info.dwSize.X-8, info.dwSize.Y-2);
+    printf("vidas");
+    gotoxy(info.dwSize.X-10, info.dwSize.Y-1);
+    for (int i=0; i<vidas; i++){
+        printf("<3 ");
+    }
+    textcolor(PADRAO);
+}
+
+void dano(int alvo, int x[], int y[], int score, int onda, int vidas, char palavras[26][50]){
+    int controle=0;
+    for (int i=0; i<2;i++){
+        system("color 44");
+        Sleep(100);
+        system("color CC");
+        Sleep(100);
+        system("color FF");
+        Sleep(100);}
+    system("color 07");
+    for (controle; controle<26; controle++){
+         if (gotoxy(x[controle], y[controle])== 0){
+            if (alvo==controle){textcolor(AMARELO_CLARO);}
+                printf("%s", palavras[controle]);
+                textcolor(PADRAO);}
+    }
+    placar(1, score, onda, vidas);
+}
+
+
 
 int main() {
     srand(time(NULL));
     char input;
-    int alvo=0; int letra=1;
-    int onda= 1, timer=0;
+    int alvo=0; int letra=1; int score=0; int vidas=3;
+    char palavras[26][50];
+    unsigned onda= 1, timer=0;
     int coordX[26];
     int coordY[26];
-    char palavras[26][50];
     int controle=0;
-    FILE *dicio;
+    /*FILE *dicio;
     dicio= fopen("dicionario.txt", "r");
     if (dicio==NULL){
         printf("erro dicionario aberto incorretamente");
         return 1;
     }
+    fclose(dicio);*/
     system("cls");
     HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
         CONSOLE_SCREEN_BUFFER_INFO info;
         GetConsoleScreenBufferInfo(h, &info);
         int x= (info.dwSize.X)/2; 
         int y =(info.dwSize.Y)/2;
-    setup(onda, coordX, coordY, palavras);
+    placar(1, score, onda, vidas);
         while (1){
-            int tempo= clock();
+            placar(0, score, onda, vidas);
+            unsigned tempo= clock();
             tempo=tempo/CLOCKS_PER_SEC;
-            // função de esclher palavras e coloca-las no char
-            textcolor(PADRAO);
-            gotoxy(x,y);
-            printf("                                     ");
+            setup(onda, coordX, coordY, palavras, x, y);
+            //display
             if((tempo-timer)> 1){
-            for (controle; controle<26; controle++){
+                
+            for (controle=0; controle<26; controle++){
                 timer= tempo;
-                if (gotoxy(coordX[controle], coordY[controle])== 0){
-                    if (alvo==controle){textcolor(AMARELO_CLARO);}
-                    printf("%s", palavras[controle]);
-                    textcolor(PADRAO);} 
+                if(coordY[controle]<info.dwSize.Y-3){
+                    if (gotoxy(coordX[controle], coordY[controle])== 0){
+                        if (alvo==controle){textcolor(AMARELO_CLARO);}
+                        printf("%s", palavras[controle]);
+                        textcolor(PADRAO);} 
                     coordY[controle]++;
+                }else{
+                    vidas--;
+                    strcpy(palavras[controle], "");
+                    dano(alvo, coordX,coordY, score, onda, vidas, palavras);
+                }
+                    
                 }   
             }  
+            //escrever palavras
             if(kbhit()){
                 input=getch();
                 if (alvo!=-1){
@@ -122,10 +174,12 @@ int main() {
                         letra++;
                         if (letra<strlen(palavras[alvo])){
                             alvo=-1;
+                            letra=1;
+                            score += (10*strlen(palavras[alvo]));
                         }
                     }
                 }
                 
             }
-    }    
+    }
 }
